b78f370d91134d0cf08979ff423efb6cc71624b8
Your Name <andy7037@naver.com>
2020-08-28 16:07:01 +0900

    JWT 토큰 localstorage 저장 완료

--- src/pages/GoogleLoginTest.js
+++ src/pages/GoogleLoginTest.js
@@ -10,27 +10,23 @@ const Config = {
 
 const responseGoogle = async (response) => {
   console.log(response);
-
   // ui를 같이 던질 수 있음 (ex. 프로그레스바)
   // fetch보다 axios를 사용하는 것이 좋음!
-
- // let res = {...response, ...{provider : "google"}} // 미리 provider를 붙여놓으면 나중에 구분하기 쉽다.
-  let jwtToken = 
-  await axios
-    .post(
-      "http://localhost:8080/oauth/jwt/google", // 스프링 컨트롤러 생성 // 주소
-      JSON.stringify(response), // response는 자바스크립트 오브젝트로 바꼈기 때문에 다시 json으로 바꿔준다. //바디
-      Config // 옵션
-      )
-      if(jwtToken.status === 200){
-          console.log(2, jwtToken.data) // 토큰
-            localStorage.setItem("jwtToken", jwtToken.data) // 이렇게 하면 웹 브라우저에 저장
-      }
-    // .then((res) => {
-    //   console.log(1, res.headers);
-    //   console.log(2, res.data);
-    //   localStorage.setItem("jwt", res.data);
-    // });
+  // let res = {...response, ...{provider : "google"}} // 미리 provider를 붙여놓으면 나중에 구분하기 쉽다.
+  let jwtToken = await axios.post(
+    "http://localhost:8080/oauth/jwt/google", // 스프링 컨트롤러 생성 // 주소
+    JSON.stringify(response), // response는 자바스크립트 오브젝트로 바꼈기 때문에 다시 json으로 바꿔준다. //바디
+    Config // 옵션
+  );
+  if (jwtToken.status === 200) {
+    console.log(2, jwtToken.data); // 토큰
+    localStorage.setItem("jwtToken", jwtToken.data); // 이렇게 하면 웹 브라우저에 저장
+  }
+  // .then((res) => {
+  //   console.log(1, res.headers);
+  //   console.log(2, res.data);
+  //   localStorage.setItem("jwt", res.data);
+  // });
 };
 
 const GoogleLoginTest = () => {
--- src/pages/SocialLogin.js
+++ src/pages/SocialLogin.js
@@ -1,51 +1,66 @@
 import React from "react";
-import { KakaoIcon } from './SocialIcons';
+import { KakaoIcon } from "./SocialIcons";
 
 const { Kakao } = window;
 function SocialLogin() {
   const kakaoLoginClickHandler = () => {
     Kakao.Auth.login({
       success: function (authObj) {
-        console.log('authObj : ', authObj);
+        console.log("authObj : ", authObj);
         Kakao.API.request({
-          url: '/v2/user/me',
-          success: function(response) {
-              console.log('닉네임 : ', response.kakao_account.profile.nickname);
-              console.log('이메일 : ', response.kakao_account.email);
-              console.log('id : ', response.id);
-              let jwtToken = fetch('http://192.168.0.101:8080/brunch/oauth/jwt/kakao',{
-                method:"post",
-                headers: {
-                  "Content-Type": "application/json; charset:utf-8",
-                  // "Authorization":"Bearer "+ authObj.access_token
-                },
-                body: JSON.stringify(response),
+          url: "/v2/user/me",
+          success: function (response) {
+            console.log("닉네임 : ", response.kakao_account.profile.nickname);
+            console.log("이메일 : ", response.kakao_account.email);
+            console.log("id : ", response.id);
+            fetch("http://192.168.0.101:8080/brunch/oauth/jwt/kakao", {
+              method: "post",
+              headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                // "Authorization":"Bearer "+ authObj.access_token
+              },
+              body: JSON.stringify(response),
+              dataType: "text/plain",
+            })
+              .then(function (jwtToken) {
+                // console.log(jwtToken.text());
+                // let test = jwtToken.sj
+                // console.log(test);
+                return jwtToken.text();
               })
-              if(jwtToken.status === 200){
-                console.log(2, jwtToken.data) // 토큰
-                  localStorage.setItem("jwtToken", jwtToken.data) // 이렇게 하면 웹 브라우저에 저장
-            }
+              .then(function (jwtTokenResult) {
+                console.log(jwtTokenResult);
+                localStorage.setItem(
+                  "Authentication",
+                  "Bearer " + jwtTokenResult
+                ); // 이렇게 하면 웹 브라우저에 저장
+                // localStorage.setItem("jwtToken", jwtToken);
+              });
+            // console.log(jwtToken);
+            // if (jwtToken.status === 200) {
+            //   console.log(2, jwtToken.data); // 토큰
+            //   localStorage.setItem("jwtToken", jwtToken.data); // 이렇게 하면 웹 브라우저에 저장
+            // }
           },
-          fail: function(error) {
-              console.log(error);
-          }
-      });
-      }
+          fail: function (error) {
+            console.log(error);
+          },
+        });
+      },
     });
-}
+  };
   return (
     <article className="socialLogin">
-      <button className="btn kakao"
-        onClick={kakaoLoginClickHandler}>
+      <button className="btn kakao" onClick={kakaoLoginClickHandler}>
         <div className="kakaoIcon">
-            {/* <KakaoLogin 
+          {/* <KakaoLogin 
             jsKey="86eb9b2d5599d4ed8a21ba6a5b047c45"
             buttonText="Login"/> */}
           <KakaoIcon />
         </div>
       </button>
-    </article >
-  )
+    </article>
+  );
 }
 
 export default SocialLogin;
